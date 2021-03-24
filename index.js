afadf4ea6e112de876698340d9353f718b45c40a
LionLuke <39203615+LionLuke@users.noreply.github.com>
2021-03-24 17:26:27 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,19 +1,25 @@
 const RPC = require('discord-rpc');
 const client = new RPC.Client({ transport: 'ipc' });
+Array.prototype.random = function() {
+  return this[Math.floor(Math.random() * this.length)];
+};
+const A = ["Maintaining the peace", "Attempting to teach", "Teaching JavaScript", "Being made fun of", "ReferenceError: LionLuke is not defined"];
 client.on('ready', () =>
 {
+	setInterval(function() {
     client.request('SET_ACTIVITY', {
         pid: process.pid,
         activity: {
             assets: {
-                large_image: 'jsu'
+                large_image: 'jsu_logo__'
             },
-            details: 'Learning JavaScript',
+            details: A.random(),
             buttons: [
-                { label: 'Join Me!', url: 'https://discord.gg/gRxgAWA' }
+                { label: 'Join', url: 'https://discord.gg/gRxgAWA' }
             ]
         }
     });
+	}, 15000)
     console.log('started!');
 });
-client.login({ clientId: '777146258888327188' });
+client.login({ clientId: '671778487761371157' });
